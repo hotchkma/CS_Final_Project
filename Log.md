66f7d3537d8426711fea925492c04099a607f37b
Ryan Ho <ryanho@Ryans-MacBook-Pro.local>
2020-06-03 14:59:41 -1000

    created views folder

--- Log.md
+++ Log.md
@@ -1,3 +1,3 @@
 * 06/03/20 Ryan
 - started server.js
--  
+- created various file folders
--- server.js
+++ server.js
@@ -4,8 +4,9 @@ var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+
+//app.engine('handlebars', exphbs({ defaultLayout: 'null'}));
+//app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
@@ -15,4 +16,8 @@ app.get('/', function (req, res) {
     twitData: tData
   });
   */
+});
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
 });
