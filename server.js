ecef0e6b5e8992572adb7a7170f2fb7cf1c845c3
Matthew Hotchkiss <hotchkma@oregonstate.edu>
2020-06-07 16:13:23 -0700

    implemented a partial for a post

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var postData = require('./posts');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+
 app.get('/', function (req, res) {
   //res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
-   res.status(200).render('home');
+   res.status(200).render('home', {
+	posts: postData
+   });
 });
 
 app.post('/:person', function( req, res, next) {
